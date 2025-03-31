bbd44a37efaa6051b955a3eeacc727b59500dcdf
Brahian-A <10114@holbertonstudents.com>
2025-03-30 21:07:00 -0300

    task 0

--- ES6_basic/0-main.js
+++ ES6_basic/0-main.js
@@ -1,15 +1,3 @@
-export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+import { taskFirst, taskNext } from './0-constants.js';
+
+console.log(`${taskFirst()} ${taskNext()}`);
