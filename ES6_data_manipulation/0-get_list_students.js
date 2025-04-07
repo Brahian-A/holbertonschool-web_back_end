d5c3556a5d784781a0d45b01aa674008dd2612d3
Brahian-A <10114@holbertonstudents.com>
2025-04-06 21:57:57 -0300

    0 and 1

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,9 @@
 const arrayOfObjects = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-  
-  export default function getListStudents()
-  {
-    return arrayOfObjects;
-  }
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+export default function getListStudents() {
+  return arrayOfObjects;
+}
