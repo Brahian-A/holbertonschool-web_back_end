14b69f691f2eeacd784c5cad44fa46633ce402a5
Brahian-A <10114@holbertonstudents.com>
2025-04-06 21:47:19 -0300

    0 and 1

---
+++ 0-get_list_students.js
@@ -0,0 +1,10 @@
+const arrayOfObjects = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  
+  export default function getListStudents()
+  {
+    return arrayOfObjects;
+  }
