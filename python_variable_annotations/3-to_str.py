468126738ed067b8a6a415624ff1e3f06273d91d
Brahian-A <10114@holbertonstudents.com>
2025-07-24 14:25:30 -0300

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Módulo que convierte un número flotante a su representación en cadena"""
+
+
+def to_str(n: float) -> str:
+    """Convierte un número float a string"""
+    return str(n)
