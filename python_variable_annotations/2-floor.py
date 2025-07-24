752f274922cb5809d8176a6a650acb73b91f97da
Brahian-A <10114@holbertonstudents.com>
2025-07-24 13:57:48 -0300

    task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""Módulo que devuelve el piso (parte entera inferior) de un flotante"""
+
+
+def floor(n: float) -> int:
+    """Funcion que retorna el piso de un flot"""
+    return math.floor(n)
