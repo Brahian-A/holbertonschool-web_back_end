358cba9f24e6f6bb1338e6b5d70cff5d8478b80a
Brahian-A <10114@holbertonstudents.com>
2025-07-25 21:56:39 -0300

    ahora?

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Módulo que suma los datos de una lista de flotantes."""
 
+
 from typing import List
 
 def sum_list(input_list: List[float]) -> float:
-    """Función que devuelve la suma de los elementos de una lista de flotantes."""
+    """Función que devuelve la suma de los elementos de una lista"""
     return sum(input_list)
