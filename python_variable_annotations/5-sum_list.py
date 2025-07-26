5a4771c2d1df67625e9b48df2d1b441ff10d4064
Brahian-A <10114@holbertonstudents.com>
2025-07-25 21:53:48 -0300

    ahora?

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,9 @@
 """ Modulo que suma los datos de una listas"""
 
 
+from typing import List
+
+
 def sum_list(input_list: list[float]) -> float:
     "Funcion que usa la funcion suma con los datos de la lista"
     return sum(input_list)
