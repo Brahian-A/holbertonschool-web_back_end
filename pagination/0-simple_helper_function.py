9de93df21c3056be76f308ac19c44293bf0808d2
Brahia-A <10114@holbertonstudents.com>
2025-08-14 10:50:07 -0300

    documentacion

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""Madulo donde se calcula la paginacion"""
+
 
 def index_range(page: int, page_size: int):
+    """funcion que calcula la paginacion"""
     inicio = ((page - 1) * page_size)
     fin = page * page_size
     return inicio, fin
